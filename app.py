e53e73a984232d708dacc93c3abd914db31695ce
Darian8 <130746790+moonloadermic@users.noreply.github.com>
2023-08-05 14:00:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.firefox import GeckoDriverManager
 
-URL = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
-XPATH = "//*[@class='ui-mainview-block eventpath-wrapper']"
-TIMEOUT = 20
+URL = "http://github.com"
+
+
 
 st.title("Test Selenium")
 st.markdown("You should see some random Football match text below in about 21 seconds")
@@ -26,16 +26,7 @@ driver = webdriver.Firefox(
 )
 driver.get(URL)
 
-try:
-    WebDriverWait(driver, TIMEOUT).until(
-        EC.visibility_of_element_located((By.XPATH, XPATH,))
-    )
+time.sleep(3)
 
-except TimeoutException:
-    st.warning("Timed out waiting for page to load")
-    driver.quit()
+st.write(driver.title)
 
-time.sleep(10)
-elements = driver.find_elements_by_xpath(XPATH)
-st.write([el.text for el in elements])
-driver.quit()
